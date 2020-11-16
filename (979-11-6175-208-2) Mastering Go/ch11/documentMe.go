790e7de129fc2b094ff9ae2ed1ed44645f6f8fae
Yuchan Lee <dldbcks95@gmail.com>
2020-11-16 16:32:33 +0900

    Studied 'Mastering Go' ch11

---
+++ (979-11-6175-208-2) Mastering Go/ch11/documentMe.go
@@ -0,0 +1,26 @@
+// For testing Go documentation check
+// no special functionalities
+package documentMe
+
+// Pie is global variable
+// -
+const Pie = 3.1415912
+
+// S1() get length of given string
+// use range to loop over string
+func S1(s string) int {
+	if s == "" {
+		return 0
+	}
+	n := 0
+	for range s {
+		n++
+	}
+	return n
+}
+
+// F1() returns twice of given integer
+// Double() would be better naming
+func F1(n int) int {
+	return 2 * n
+}
