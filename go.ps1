008a789c786076b23030076f12ef3624e641eb67
Yuchan Lee <dldbcks95@gmail.com>
2020-11-25 18:16:45 +0900

    Embed go.Dockerfile into go.ps1
    To reduce number of files, embeded dockerfile content in powershell script.

--- go.ps1
+++ go.ps1
@@ -1,11 +1,38 @@
+# Image name to be used
 $image = "algorithm_go:local"
+ 
+# Embedded dockerfile
+$dockerfile = `
+'FROM golang:1.15
 
-docker image build -f "go.Dockerfile" -t $image .
+USER root
+VOLUME /workspace
+
+RUN apt update && \
+    apt install -y \
+        vim \
+        strace \
+        dos2unix
+
+RUN go get -v golang.org/x/lint/golint
+
+WORKDIR /workspace
+
+CMD ["/bin/bash"]'
+
+# Create temporary dockerfile
+$temp = New-TemporaryFile
+Set-Content -Path $temp -Value $dockerfile
+
+# Build image with temporary dockerfile
+docker image build -f $temp -t $image .
 echo ""
 
+# Run
 docker container run -it --rm `
     -v ${PWD}:/workspace `
     -w /workspace `
     $image /bin/bash
 
+# Exited container; give user some time to see outputs
 pause
